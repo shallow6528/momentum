6f12f3681a749307061f51d993db0522a3c32bd8
shallow6528 <shallow6528@gmail.com>
2022-12-23 22:53:06 +0900

    momentum - 주석 제거

--- js/background.js
+++ js/background.js
@@ -1,12 +1,3 @@
-// const images = ["0.jpg", "1.jpg", "2.jpg"];
-// const chosenImage = images[Math.floor(Math.random() * images.length)];
-// const bgImage = document.createElement("img");
-
-// bgImage.src = `img/${chosenImage}`;
-
-// document.body.appendChild(bgImage);
-
-
 const images = ["0.jpg", "1.jpg", "2.jpg"];
 const img = images[Math.floor(Math.random() * images.length)];
 const imgSrc = "./img/" + img;
